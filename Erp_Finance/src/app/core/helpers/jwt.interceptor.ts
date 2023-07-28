808aed2194b6fbd31081ed7ea3aa5d0f40bf8061
Hari Krishna Prasad T <hariofficial137@gmail.com>
2023-07-28 14:22:28 +0530

    jwt

--- Erp_Finance/src/app/core/helpers/jwt.interceptor.ts
+++ Erp_Finance/src/app/core/helpers/jwt.interceptor.ts
@@ -13,24 +13,26 @@ export class JwtInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         if (environment.defaultauth === 'firebase') {
-            const currentUser = this.authenticationService.currentUser();
-            if (currentUser && currentUser.token) {
-                request = request.clone({
-                    setHeaders: {
-                        Authorization: `Bearer ${currentUser.token}`
-                    }
-                });
-            }
+            // const currentUser = this.authenticationService.currentUser();
+            const token = "";
+            // if (currentUser && currentUser.token) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            // }
         } else {
             // add authorization header with jwt token if available
-            const currentUser = this.authfackservice.currentUserValue;
-            if (currentUser && currentUser.token) {
-                request = request.clone({
-                    setHeaders: {
-                        Authorization: `Bearer ${currentUser.token}`
-                    }
-                });
-            }
+            // const currentUser = this.authfackservice.currentUserValue;
+            const token = "";
+            // if (currentUser && currentUser.token) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            // }
         }
         return next.handle(request);
     }
